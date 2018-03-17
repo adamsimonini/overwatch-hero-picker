15adc590dc593923be4dd8e202fa64fa2a0c833e
Adam Simonini <adamsimonini@users.noreply.github.com>
2018-03-17 16:08:44 -0400

    Handle Spaces in Company name

--- teamFiller.js
+++ teamFiller.js
@@ -1,8 +1,8 @@
 function teamFiller(url){
     if(url.indexOf('++') >= 0){
         var split = url.split('++');
-        var companyName = split[0].slice(split[0].indexOf("?") + 1);
-        document.getElementById('company').value = companyName;
+        var companyName = split[0].slice(split[0].indexOf("?") + 1).replace(/%20/g, " ") ;
+        document.getElementById('company').value= companyName;
         var teamArray = split[1].split('+');
         for(i=0;i<team.length;i++){
             team[i].setAttribute("hero", teamArray[i]);
